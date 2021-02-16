886cc16c5cba6067d21d77cc3ef395a009fe4f36
Chris <who7708@163.com>
2021-02-16 18:11:02 +0800

    init

---
+++ mina-server/src/main/java/com/example/server/MinaService.java
@@ -0,0 +1,73 @@
+package com.example.server;
+
+import android.util.Log;
+
+import org.apache.commons.lang3.time.FastDateFormat;
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.serialization.ObjectSerializationCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Chris
+ * @version 1.0.0
+ * @date 2/16/21
+ */
+public class MinaService {
+
+    private static final String TAG = "MinaService";
+
+    public static void main(String[] args) {
+        IoAcceptor ioAcceptor = new NioSocketAcceptor();
+        // 添加日志过滤器
+        ioAcceptor.getFilterChain().addLast("logger", new LoggingFilter());
+        ioAcceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new ObjectSerializationCodecFactory()));
+        ioAcceptor.setHandler(new DemoServerHandler());
+        ioAcceptor.getSessionConfig().setReadBufferSize(2048);
+        ioAcceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+        try {
+            ioAcceptor.bind(new InetSocketAddress(9123));
+        } catch (Exception e) {
+            Log.i(TAG, "main: ", e);
+        }
+    }
+
+    /**
+     * 负责session对象的创建监听及消息发送、接收的监听
+     */
+    private static class DemoServerHandler extends IoHandlerAdapter {
+        @Override
+        public void sessionCreated(IoSession session) throws Exception {
+            super.sessionCreated(session);
+        }
+
+        @Override
+        public void sessionOpened(IoSession session) throws Exception {
+            super.sessionOpened(session);
+        }
+
+        @Override
+        public void sessionClosed(IoSession session) throws Exception {
+            super.sessionClosed(session);
+        }
+
+        @Override
+        public void messageReceived(IoSession session, Object message) throws Exception {
+            super.messageReceived(session, message);
+            String str = message.toString();
+            String date = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss.SSS").format(System.currentTimeMillis());
+            session.write(date);
+        }
+
+        @Override
+        public void messageSent(IoSession session, Object message) throws Exception {
+            super.messageSent(session, message);
+        }
+    }
+}
